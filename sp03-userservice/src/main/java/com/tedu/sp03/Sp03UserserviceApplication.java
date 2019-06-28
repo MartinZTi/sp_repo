bc0ecca39f16352697bc1a41fb9590b80208e786
MartinZTi <50127538+MartinZTi@users.noreply.github.com>
2019-06-28 10:26:41 +0800

    Update Sp03UserserviceApplication.java

--- sp03-userservice/src/main/java/com/tedu/sp03/Sp03UserserviceApplication.java
+++ sp03-userservice/src/main/java/com/tedu/sp03/Sp03UserserviceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class Sp03UserserviceApplication {
 //大是大非大
+	//大彻大悟
 	public static void main(String[] args) {
 		SpringApplication.run(Sp03UserserviceApplication.class, args);
 	}
