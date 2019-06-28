925254d2cf3ea4bc65323cf2ad2e14c7c61fe94a
MZT <2998816164@qq.com>
2019-06-28 10:18:56 +0800

    写了注释

--- sp03-userservice/src/main/java/com/tedu/sp03/Sp03UserserviceApplication.java
+++ sp03-userservice/src/main/java/com/tedu/sp03/Sp03UserserviceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Sp03UserserviceApplication {
-
+//大是大非大
 	public static void main(String[] args) {
 		SpringApplication.run(Sp03UserserviceApplication.class, args);
 	}
